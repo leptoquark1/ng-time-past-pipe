5ca4bf22a3fd947b5ae7a8970b9092e1ac587813
David Fecke <david.fecke@eyecook.net>
2021-01-22 22:37:26 +0100

    fix: add working example in playground

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Playground';
+  date1 = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgTimePastPipeModule } from 'ng-time-past-pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgTimePastPipeModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
