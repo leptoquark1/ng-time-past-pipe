453e3291f9f642cc7c4907b7217a8681c373e25f
David Fecke <david.fecke@eyecook.net>
2021-01-30 21:41:42 +0100

    chore: playground module adjustments
    - remove spec files and test commands
    - switch to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tpp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   date1 = new Date();
